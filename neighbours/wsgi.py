2381781f065c94a9ecedf95b04cc3bba02845b2b
Cyci25 <c2muriithi@gmail.com>
2019-01-16 14:24:25 +0300

    updates

--- neighbours/wsgi.py
+++ neighbours/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for neighbours project.
+WSGI config for theneighbourhoodproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neighbours.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theneighbourhoodproject.settings")
 
 application = get_wsgi_application()
